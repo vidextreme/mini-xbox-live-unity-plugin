86cf0eec02f82eca6684ec46a3a83bd7d19ab02e
david <vidextreme@yahoo.com>
2020-08-11 01:21:50 +0800

    still not working

---
+++ Editor/GameSaveStatus.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) John David Bonifacio Uy
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+//Values taken from https://docs.microsoft.com/en-us/uwp/api/windows.gaming.xboxlive.storage.gamesaveerrorstatus?view=winrt-19041
+namespace Microsoft.Xbox.Services.ConnectedStorage
+{
+    public enum GameSaveStatus
+    {
+        Abort                   = -2147467260,
+        BlobNotFound            = -2138898424,
+        ContainerNotInSync      = -2138898422,
+        ContainerSyncFailed     = -2138898421,
+        InvalidContainerName    = -2138898431,
+        NoAccess                = -2138898430,
+        NoXboxLiveInfo          = -2138898423,
+        ObjectExpired           = -2138898419,
+        Ok                      = 0,
+        OutOfLocalStorage       = -2138898429,
+        ProvidedBufferTooSmall  = -2138898425,
+        QuotaExceeded           = -2138898426,
+        UpdateTooBig            = -2138898427,
+        UserCanceled            = -2138898428,
+        UserHasNoXboxLiveInfo   = -2138898420,
+    }
+}
+
---
+++ Editor/UnityCommonExtensions.cs
@@ -0,0 +1,82 @@
+﻿// Copyright (c) John David Bonifacio Uy
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// 
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class UnityCommonExtensions
+{
+    public static byte[] ToByteArray(this Vector4 v)
+    {
+        byte[] buff = new byte[16]; // 4 bytes per float
+
+        Buffer.BlockCopy(BitConverter.GetBytes(v.x), 0, buff, 0, 4);
+        Buffer.BlockCopy(BitConverter.GetBytes(v.y), 0, buff, 4, 4);
+        Buffer.BlockCopy(BitConverter.GetBytes(v.z), 0, buff, 8, 4);
+        Buffer.BlockCopy(BitConverter.GetBytes(v.w), 0, buff, 12, 4);
+        return buff;
+    }
+    public static byte[] ToByteArray(this Quaternion v)
+    {
+        byte[] buff = new byte[16]; // 4 bytes per float
+
+        Buffer.BlockCopy(BitConverter.GetBytes(v.x), 0, buff, 0, 4);
+        Buffer.BlockCopy(BitConverter.GetBytes(v.y), 0, buff, 4, 4);
+        Buffer.BlockCopy(BitConverter.GetBytes(v.z), 0, buff, 8, 4);
+        Buffer.BlockCopy(BitConverter.GetBytes(v.w), 0, buff, 12, 4);
+        return buff;
+    }
+    public static byte[] ToByteArray(this Vector3 v)
+    {
+        byte[] buff = new byte[12]; // 4 bytes per float
+
+        Buffer.BlockCopy(BitConverter.GetBytes(v.x), 0, buff, 0, 4);
+        Buffer.BlockCopy(BitConverter.GetBytes(v.y), 0, buff, 4, 4);
+        Buffer.BlockCopy(BitConverter.GetBytes(v.z), 0, buff, 8, 4);
+        return buff;
+    }
+    public static byte[] ToByteArray(this Vector2 v)
+    {
+        byte[] buff = new byte[8]; // 4 bytes per float
+
+        Buffer.BlockCopy(BitConverter.GetBytes(v.x), 0, buff, 0, 4);
+        Buffer.BlockCopy(BitConverter.GetBytes(v.y), 0, buff, 4, 4);
+        return buff;
+    }
+
+    public static Quaternion ToQuaternion(this byte[] buff)
+    {
+        Quaternion v = new Quaternion();
+        v.x = BitConverter.ToSingle(buff, 0 * sizeof(float));
+        v.y = BitConverter.ToSingle(buff, 1 * sizeof(float));
+        v.z = BitConverter.ToSingle(buff, 2 * sizeof(float));
+        v.w = BitConverter.ToSingle(buff, 3 * sizeof(float));
+        return v;
+    }
+    public static Vector3 ToVector4(this byte[] buff)
+    {
+        Vector4 v = new Vector4();
+        v.x = BitConverter.ToSingle(buff, 0 * sizeof(float));
+        v.y = BitConverter.ToSingle(buff, 1 * sizeof(float));
+        v.z = BitConverter.ToSingle(buff, 2 * sizeof(float));
+        v.w = BitConverter.ToSingle(buff, 3 * sizeof(float));
+        return v;
+    }
+    public static Vector3 ToVector3(this byte[] buff)
+    {
+        Vector3 v = new Vector3();
+        v.x = BitConverter.ToSingle(buff, 0 * sizeof(float));
+        v.y = BitConverter.ToSingle(buff, 1 * sizeof(float));
+        v.z = BitConverter.ToSingle(buff, 2 * sizeof(float));
+        return v;
+    }
+    public static Vector2 ToVector2(this byte[] buff)
+    {
+        Vector2 v = new Vector2();
+        v.x = BitConverter.ToSingle(buff, 0 * sizeof(float));
+        v.y = BitConverter.ToSingle(buff, 1 * sizeof(float));
+        return v;
+    }
+}
